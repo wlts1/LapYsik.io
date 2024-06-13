bb4132c9ce6a4a054d93c43ef61e8ec82c9c310f
wlts1 <71607797+wlts1@users.noreply.github.com>
2024-06-13 21:46:48 +0300

    Update cart.php

--- cart.php
+++ cart.php
@@ -39,47 +39,49 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['clear_cart'])) {
     <link rel="manifest" href="assets/pictures/site.webmanifest">
 </head>
 <body>
-    <div class="header">
-        <div class="container">
-            <div class="header_line">
-                <div class="header_logo">
-                    <img src="assets/pictures/logo.png" alt="Логотип магазина кормов">
-                </div>
-                <div class="nav">
-                    <a class="nav_item" href="index.php">ГЛАВНАЯ</a>
-                    <a class="nav_item" href="catalog.php">КАТАЛОГ</a>
+    <div class="page">
+        <div class="header">
+            <div class="container">
+                <div class="header_line">
+                    <div class="header_logo">
+                        <img src="assets/pictures/logo.png" alt="Логотип магазина кормов">
+                    </div>
+                    <div class="nav">
+                        <a class="nav_item" href="index.php">ГЛАВНАЯ</a>
+                        <a class="nav_item" href="catalog.php">КАТАЛОГ</a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="cart_container">
-            <div class="cart_title">Ваша корзина</div>
-            <?php if (empty($cart)): ?>
-                <div class="cart_text">Ваша корзина пуста.</div>
-                <a class="btn" href="catalog.php">Добавить товары</a>
-            <?php else: ?>
-                <div class="cart_list">
-                    <?php foreach ($cart as $item): ?>
-                        <li class="cart_item">
-                            <div class="product_title"><?= htmlspecialchars($item['Название']) ?></div>
-                            <div class="product_desc"><?= htmlspecialchars($item['Описание']) ?></div>
-                            <div class="product_price">Цена: <?= htmlspecialchars($item['Цена']) ?> руб.</div>
-                            <div class="product_quantity">Количество: <?= htmlspecialchars($item['quantity']) ?></div>
-                        </li>
-                    <?php endforeach; ?>
-                </div>
-                <div class="cart_forms">
-                    <div class="cart_form">
-                        <form action="order.php" method="post">
-                            <button type="submit" class="btn">Оформить заказ</button>
-                        </form>
+            <div class="cart_container">
+                <div class="cart_title">Ваша корзина</div>
+                <?php if (empty($cart)): ?>
+                    <div class="cart_text">Ваша корзина пуста.</div>
+                    <a class="btn" href="catalog.php">Добавить товары</a>
+                <?php else: ?>
+                    <div class="cart_list">
+                        <?php foreach ($cart as $item): ?>
+                            <li class="cart_item">
+                                <div class="product_title"><?= htmlspecialchars($item['Название']) ?></div>
+                                <div class="product_desc"><?= htmlspecialchars($item['Описание']) ?></div>
+                                <div class="product_price">Цена: <?= htmlspecialchars($item['Цена']) ?> руб.</div>
+                                <div class="product_quantity">Количество: <?= htmlspecialchars($item['quantity']) ?></div>
+                            </li>
+                        <?php endforeach; ?>
                     </div>
-                    <div class="cart_form">
-                        <form action="cart.php" method="post">
-                            <button type="submit" name="clear_cart" class="btn">Очистить корзину</button>
-                        </form>
+                    <div class="cart_forms">
+                        <div class="cart_form">
+                            <form action="order.php" method="post">
+                                <button type="submit" class="btn">Оформить заказ</button>
+                            </form>
+                        </div>
+                        <div class="cart_form">
+                            <form action="cart.php" method="post">
+                                <button type="submit" name="clear_cart" class="btn">Очистить корзину</button>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            <?php endif; ?>
+                <?php endif; ?>
+            </div>
         </div>
     </div>
 </body>
